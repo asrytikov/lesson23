499c91f90ac9aba70be0271c39465639aff57da7
asrytikov <asrytikov@gmail.com>
2022-11-28 21:24:48 +0400

    Lesson23 - branch test l1

--- src/main/java/lesson232/Lesson23Application.java
+++ src/main/java/lesson232/Lesson23Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lesson23Application {
-    //branch test l1
     public static void main(String[] args) {
         SpringApplication.run(Lesson23Application.class, args);
     }
